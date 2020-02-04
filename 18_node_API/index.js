d318cd124ddb27c17fcd123ddd66780e35df545f
asosunoff <al.sosunoff@gmail.com>
2020-02-04 10:53:51 +0300

    update

--- 18_node_API/index.js
+++ 18_node_API/index.js
@@ -2,84 +2,138 @@ const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
+const promise = require('./src/getPromise');
+
 const server = express();
+const apiRouter = express.Router();
 let jsonParser = bodyParser.json();
-server.use(express.static(__dirname + '/public'));
 
-server.get('/api/users', (req, res) => {
-    let content = fs.readFileSync('src/users.json', 'utf8');
-    let users = JSON.parse(content);
-    res.send(users);
+server
+    .use(express.static(__dirname + '/public'))
+    .listen(3000);
+
+apiRouter.get('/users', (req, res) => {
+    fs.readFile('src/users.json', 'utf8', (err, data) => {
+        if(err)
+            res.sendStatus(400);
+            
+        let users = JSON.parse(data);
+        res.send(users);
+    });
 });
 
-server.get('/api/user/:id', (req, res) => {
+apiRouter.get('/user/:id', (req, res) => {
     let id = req.params['id'];
-    let content = fs.readFileSync('src/users.json', 'utf8');
-    let users = JSON.parse(content);
-    let user = users.find(e => e.id == id);
-    if(user)
-        res.send(user);
-    else
-        res.sendStatus(400);
+    fs.readFile('src/users.json', 'utf8', (err, data) => {
+        if(err)
+            res.sendStatus(400);
+
+        let users = JSON.parse(data);
+        let user = users.find(e => e.id == id);
+        if(user)
+            res.send(user);
+        else
+            res.sendStatus(400);
+    });   
 });
 
-server.post('/api/user', jsonParser, (req, res) => {
+apiRouter.post('/user', jsonParser, (req, res) => {
     if(!req.body) return res.sendStatus(400);
 
     let user = {
         name: req.body.name, 
         age: req.body.age
     };
-     
-    let content = fs.readFileSync("src/users.json", "utf8");
-    let users = JSON.parse(content);
-     
-    let id = Math.max.apply(Math, users.map(o => o.id));
-    user.id = ++id;
-    users.push(user);
-    var newData = JSON.stringify(users);
     
-    fs.writeFileSync("src/users.json", newData);
+    promise((resolve, reject) => {
+        fs.readFile("src/users.json", "utf8", (err, data) => {
+            if(err)
+                reject(err);
+    
+            let users = JSON.parse(data);
+    
+            let id = Math.max.apply(Math, users.map(o => o.id));
+            user.id = ++id;
+            users.push(user);
+            var newData = JSON.stringify(users);
 
-    res.send(user);
+            resolve({
+                newData: newData,
+                user: user
+            });
+        });
+    }).then(data => {
+        fs.writeFile("src/users.json", data.newData, () => {
+            res.send(data.user);
+        });
+    }).catch(err => {
+        res.sendStatus(400);
+    });  
 });
 
-server.delete("/api/user/:id", (req, res) => {
+apiRouter.delete("/user/:id", (req, res) => {
     let id = req.params['id'];
-    let data = fs.readFileSync("src/users.json", "utf8");
-    let users = JSON.parse(data);
-    let index = users.findIndex(e => e.id == id);
 
-    if(index > -1){
-        let user = users.splice(index, 1)[0];
-        let data = JSON.stringify(users);
-        fs.writeFileSync("src/users.json", data);
-        res.send(user);
-    }
-    else{
+    promise((resolve, reject) => {
+        fs.readFile("src/users.json", "utf8", (err, data) => {
+            if(err)
+                reject(err);
+    
+            let users = JSON.parse(data);
+            let index = users.findIndex(e => e.id == id);
+    
+            if(index > -1){
+                let user = users.splice(index, 1)[0];
+                let data = JSON.stringify(users);
+                resolve({
+                    users: data,
+                    user: user
+                })
+            } else {
+                reject();
+            }
+        });
+    }).then(data => {
+        fs.writeFile("src/users.json", data.users, () => {
+            res.send(data.user);
+        });
+    }).catch(err => {
         res.sendStatus(404);
-    }
+    });
 });
 
-server.put("/api/user", jsonParser, function(req, res){
+apiRouter.put("/user", jsonParser, function(req, res){
     if(!req.body) return res.sendStatus(400);
      
     let id = req.body.id;
-     
-    let content = fs.readFileSync("src/users.json", "utf8");
-    let users = JSON.parse(content);
-    let user = users.find(e => e.id == id);
+    
+    promise((resolve, reject) => {
+        fs.readFile("src/users.json", "utf8", (err, data) => {
+            if(err)
+                reject(err);
 
-    if(user){
-        user.age = req.body.age;
-        user.name = req.body.name;
-        let newData = JSON.stringify(users);
-        fs.writeFileSync("src/users.json", newData);
-        res.send(user);
-    }
-    else{
-        res.status(404).send(user);
-    }
+            let users = JSON.parse(data);
+            let user = users.find(e => e.id == id);
+
+            if(user){
+                user.age = req.body.age;
+                user.name = req.body.name;
+                let newData = JSON.stringify(users);
+                resolve({
+                    users: newData,
+                    user: user
+                });
+            } else {
+                reject();
+            }
+        });
+    }).then(data => {
+        fs.writeFile("src/users.json", data.users, () => {
+            res.send(data.user);
+        });
+    }).catch(err => {
+        res.sendStatus(400);
+    });
 });
 
-server.listen(3000);
+server.use('/api', apiRouter);
