0697711cec05301ddba3b4529602252633c0c591
asosunoff <al.sosunoff@gmail.com>
2020-02-06 16:57:13 +0300

    update

--- 24_node_API_MongoDB/src/index.js
+++ 24_node_API_MongoDB/src/index.js
@@ -2,40 +2,61 @@ const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const path = require('path');
+const {MongoClient, ObjectID} = require('mongodb');
 
 const promise = require('./getPromise');
 
 const server = express();
 const apiRouter = express.Router();
 let jsonParser = bodyParser.json();
+const mongoClient = new MongoClient('mongodb://localhost:27017',  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+});
 
-server
-    .use(express.static(path.resolve(__dirname, '../public')))
-    .listen(3000);
+let dbClient = null;
+
+mongoClient.connect((err, client) => {
+    if(err) return console.log(err);
+
+    dbClient = client;
+
+    server.locals.userCollection = client.db('24_node_API_MongoDB').collection('users');
+
+    server
+        .use(express.static(path.resolve(__dirname, '../public')))
+        .listen(3000, () => {
+            console.log('Server is started...');
+        });
+});
 
 apiRouter.get('/users', (req, res) => {
-    fs.readFile('users.json', 'utf8', (err, data) => {
-        if(err)
-            res.sendStatus(400);
-            
-        let users = JSON.parse(data);
+    req.app.locals.userCollection.find().toArray((err, users) => {
+        if(err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+
         res.send(users);
     });
 });
 
 apiRouter.get('/user/:id', (req, res) => {
-    let id = req.params['id'];
-    fs.readFile('users.json', 'utf8', (err, data) => {
-        if(err)
-            res.sendStatus(400);
+    try {
+        let id = ObjectID(req.params['id']);
 
-        let users = JSON.parse(data);
-        let user = users.find(e => e.id == id);
-        if(user)
-            res.send(user);
-        else
-            res.sendStatus(400);
-    });   
+        req.app.locals.userCollection.findOne({_id: id}, (err, result) => {
+            if(err) {
+                console.log(err);
+                return res.sendStatus(400);
+            }
+    
+            res.send(result);
+        }); 
+    } catch(err){
+        console.log(err.message);
+        res.sendStatus(404);
+    }
 });
 
 apiRouter.post('/user', jsonParser, (req, res) => {
@@ -45,96 +66,60 @@ apiRouter.post('/user', jsonParser, (req, res) => {
         name: req.body.name, 
         age: req.body.age
     };
-    
-    promise((resolve, reject) => {
-        fs.readFile("users.json", "utf8", (err, data) => {
-            if(err)
-                reject(err);
-    
-            let users = JSON.parse(data);
-    
-            let id = Math.max.apply(Math, users.map(o => o.id));
-            user.id = ++id;
-            users.push(user);
-            var newData = JSON.stringify(users);
 
-            resolve({
-                newData: newData,
-                user: user
-            });
-        });
-    }).then(data => {
-        fs.writeFile("users.json", data.newData, () => {
-            res.send(data.user);
-        });
-    }).catch(err => {
-        res.sendStatus(400);
-    });  
+    req.app.locals.userCollection.insertOne(user, (err, result) => {
+        if(err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+
+        res.send(user);
+    });
 });
 
 apiRouter.delete("/user/:id", (req, res) => {
-    let id = req.params['id'];
+    try {
+        let id = ObjectID(req.params['id']);
 
-    promise((resolve, reject) => {
-        fs.readFile("users.json", "utf8", (err, data) => {
-            if(err)
-                reject(err);
-    
-            let users = JSON.parse(data);
-            let index = users.findIndex(e => e.id == id);
-    
-            if(index > -1){
-                let user = users.splice(index, 1)[0];
-                let data = JSON.stringify(users);
-                resolve({
-                    users: data,
-                    user: user
-                })
-            } else {
-                reject();
+        req.app.locals.userCollection.findOneAndDelete({_id: id}, (err, result) => {
+            if(err) {
+                console.log(err);
+                return res.sendStatus(400);
             }
+    
+            res.send(result.value);
         });
-    }).then(data => {
-        fs.writeFile("users.json", data.users, () => {
-            res.send(data.user);
-        });
-    }).catch(err => {
+    } catch (err) {
+        console.log(err.message);
         res.sendStatus(404);
-    });
+    }
 });
 
 apiRouter.put("/user", jsonParser, function(req, res){
     if(!req.body) return res.sendStatus(400);
      
-    let id = req.body.id;
+    let id = ObjectID(req.body.id);
     
-    promise((resolve, reject) => {
-        fs.readFile("users.json", "utf8", (err, data) => {
-            if(err)
-                reject(err);
-
-            let users = JSON.parse(data);
-            let user = users.find(e => e.id == id);
-
-            if(user){
-                user.age = req.body.age;
-                user.name = req.body.name;
-                let newData = JSON.stringify(users);
-                resolve({
-                    users: newData,
-                    user: user
-                });
-            } else {
-                reject();
+    req.app.locals.userCollection.findOneAndUpdate(
+        {_id: id},
+        {$set: {
+            age: req.body.age,
+            name: req.body.name
+        }},
+        {returnOriginal: false},
+        (err, result) => {
+            if(err) {
+                console.log(err);
+                return res.sendStatus(400);
             }
+
+            res.send(result.value);
         });
-    }).then(data => {
-        fs.writeFile("users.json", data.users, () => {
-            res.send(data.user);
-        });
-    }).catch(err => {
-        res.sendStatus(400);
-    });
 });
 
 server.use('/api', apiRouter);
+
+process.on('SIGINT', () => {
+    dbClient.close();
+    process.exit();
+});
